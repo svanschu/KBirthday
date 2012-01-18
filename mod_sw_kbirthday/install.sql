88c2c7f80f0bc4fc960a3bdb84e044c791f2f3d8
Sven Schultschik <sven@schultschik.de>
2012-01-18 16:14:16 +0100

    add missing SQL for e-mail function

---
+++ mod_sw_kbirthday/install.sql
@@ -0,0 +1,12 @@
+-- @package Joomla.Module
+-- @subpackage mod_sw_kbirthday
+-- @copyright Copyright (C) 2012 Sven Schultschik. All rights reserved.
+-- @license GNU General Public License version 2 or later
+-- @link http://www.schultschik.de
+
+
+CREATE TABLE IF NOT EXISTS `pz5vo_sw_kbirthday` (
+  `uid` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  KEY `uid` (`uid`)
+);
---
+++ mod_sw_kbirthday/uninstall.sql
@@ -0,0 +1,7 @@
+-- @package Joomla.Module
+-- @subpackage mod_sw_kbirthday
+-- @copyright Copyright (C) 2012 Sven Schultschik. All rights reserved.
+-- @license GNU General Public License version 2 or later
+-- @link http://www.schultschik.de
+
+DROP TABLE pz5vo_sw_kbirthday;
