4bf9e05c3dc07559764922948acbacdc05b872eb
Sven Schultschik <sven@schultschik.de>
2013-12-02 17:38:25 +0100

    [#30]  add check if botID is 0

--- mod_sw_kbirthday/helper/forum.php
+++ mod_sw_kbirthday/helper/forum.php
@@ -10,6 +10,8 @@
 
 defined('_JEXEC') or die();
 
+jimport('joomla.log.log');
+
 class ModSWKbirthdayHelperForum extends ModSWKbirthdayHelper
 {
     /**
@@ -44,13 +46,13 @@ class ModSWKbirthdayHelperForum extends ModSWKbirthdayHelper
             //print_r($postyear->format('Y', true) .' '. $this->time_now->format('Y', true));
             //print_r('Empty Post = ' . empty($post) . '-->' . $post);
             if (empty($post) && !empty($catid) ||
-                !empty($post) && !empty($catid) && $postyear->format('Y', true) < $this->time_now->format('Y', true))
-            {
-                if( !empty($post) ) {
+                !empty($post) && !empty($catid) && $postyear->format('Y', true) < $this->time_now->format('Y', true)
+            ) {
+                if (!empty($post)) {
                     $query = $db->getQuery(true);
                     $query->delete('#__schuweb_birthday_message')
-                        ->where('userid='.$db->escape($user['userid']))
-                        ->where('topicid='.$db->escape($post['topicid']));
+                        ->where('userid=' . $db->escape($user['userid']))
+                        ->where('topicid=' . $db->escape($post['topicid']));
                     $db->setQuery($query);
                     $db->execute();
                 }
@@ -59,54 +61,59 @@ class ModSWKbirthdayHelperForum extends ModSWKbirthdayHelper
                 $message = $db->escape(self::getMessage($username));
                 $subject = $db->escape(self::getSubject($username));
 
-                $_user = KunenaUserHelper::get($botid);
-                $fields = array(
-                    'category_id' => (int)$catid,
-                    'name' => $_user->getName(''),
-                    'email' => null,
-                    'subject' => $subject,
-                    'message' => str_replace('\n', "\n", html_entity_decode($message, ENT_COMPAT, 'UTF-8')),
-                    'icon_id' => 0,
-                    'tags' => null,
-                    'mytags' => null,
-                    'subscribe' => 0,);
-                $category = KunenaForumCategoryHelper::get((int)$catid);
-                $app = JFactory::getApplication();
-                if (!$category->exists()) {
-                    $app->setUserState('com_kunena.postfields', $fields);
-                    $app->enqueueMessage($category->getError(), 'notice');
-                }
-                if (!$category->authorise('topic.create', $_user)) {
-                    $app->setUserState('com_kunena.postfields', $fields);
-                    $app->enqueueMessage($category->getError(), 'notice');
-                    //$this->redirectBack ();
-                }
-                list($topic, $message) = $category->newTopic($fields, $_user);
-                //save message
-                $success = $message->save();
-                if (!$success) {
-                    $app->enqueueMessage($message->getError(), 'error');
-                    $app->setUserState('com_kunena.postfields', $fields);
-                    //$this->redirectBack ();
-                }
-                // Display possible warnings (upload failed etc)
-                foreach ($message->getErrors() as $warning) {
-                    $app->enqueueMessage($warning, 'notice');
-                }
+                if ($botid != 0) {
+                    $_user = KunenaUserHelper::get($botid);
+                    $fields = array(
+                        'category_id' => (int)$catid,
+                        'name' => $_user->getName(''),
+                        'email' => null,
+                        'subject' => $subject,
+                        'message' => str_replace('\n', "\n", html_entity_decode($message, ENT_COMPAT, 'UTF-8')),
+                        'icon_id' => 0,
+                        'tags' => null,
+                        'mytags' => null,
+                        'subscribe' => 0,);
+                    $category = KunenaForumCategoryHelper::get((int)$catid);
+                    $app = JFactory::getApplication();
+                    if (!$category->exists()) {
+                        $app->setUserState('com_kunena.postfields', $fields);
+                        $app->enqueueMessage($category->getError(), 'notice');
+                    }
+                    if (!$category->authorise('topic.create', $_user)) {
+                        $app->setUserState('com_kunena.postfields', $fields);
+                        $app->enqueueMessage($category->getError(), 'notice');
+                        //$this->redirectBack ();
+                    }
+                    list($topic, $message) = $category->newTopic($fields, $_user);
+                    //save message
+                    $success = $message->save();
+                    if (!$success) {
+                        $app->enqueueMessage($message->getError(), 'error');
+                        $app->setUserState('com_kunena.postfields', $fields);
+                        //$this->redirectBack ();
+                    }
+                    // Display possible warnings (upload failed etc)
+                    foreach ($message->getErrors() as $warning) {
+                        $app->enqueueMessage($warning, 'notice');
+                    }
 
-                //DEBUG
-                //print_r('topic id = ' . $topic->id);die();
-                //Save the new topic to the topic user matching table
-                $query = $db->getQuery(true);
-                $query->insert('#__schuweb_birthday_message')
-                    ->set('userid = ' . $user['userid'])
-                    ->set('topicid = ' . $topic->id);
-                $db->setQuery($query);
-                $db->execute();
+                    //DEBUG
+                    //print_r('topic id = ' . $topic->id);die();
+                    //Save the new topic to the topic user matching table
+                    $query = $db->getQuery(true);
+                    $query->insert('#__schuweb_birthday_message')
+                        ->set('userid = ' . $user['userid'])
+                        ->set('topicid = ' . $topic->id);
+                    $db->setQuery($query);
+                    $db->execute();
 
 
-                //TODO alt tag
-                $user['link'] = '<a href="' . $message->getUrl($catid, true) . '">' . $username . '</a>';
+                    //TODO alt tag
+                    $user['link'] = '<a href="' . $message->getUrl($catid, true) . '">' . $username . '</a>';
+                } else {
+                    JLog::add('The user ID for the bot creating Kunena threads does not exist', JLog::WARNING);
+                    $user['link'] = $username;
+                }
 
             } elseif (!empty($post)) {
                 if (class_exists('Kunena'))
