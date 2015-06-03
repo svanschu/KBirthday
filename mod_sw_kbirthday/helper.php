e3b30d4d18d0d19fb42a61531dfc3ee90b3cd809
svanschu <sven@schultschik.de>
2015-06-03 19:53:47 +0200

    [#22] functionality to calculate the birthdays one time a day and save them into a DB table

--- mod_sw_kbirthday/helper.php
+++ mod_sw_kbirthday/helper.php
@@ -45,69 +45,16 @@ abstract class ModSWKbirthdayHelper
      */
     private function getBirthdayUser()
     {
-        $db = JFactory::getDBO();
-        $query = $db->getQuery(true);
-        $query->select('b.username');
-        $query->select('b.name');
-        $query->select('b.id AS userid');
-        $query->select('b.email');
-        $jomsocial = '';
-
-        $birthdayFields = $this->integration->getBirthdayDatabaseFields();
-
-        $query->select('YEAR(a.' . $birthdayFields['birthdate'] . ') AS year')
-            ->select('MONTH(a.' . $birthdayFields['birthdate'] . ') AS month')
-            ->select('DAYOFMONTH(a.' . $birthdayFields['birthdate'] . ') AS day')
-            ->select('DAYOFYEAR(a.' . $birthdayFields['birthdate'] . ') AS yearday')
-            ->select('DATEDIFF(DATE(a.' . $birthdayFields['birthdate'] . ') +
-                        INTERVAL(YEAR(CURDATE()) - YEAR(a.' . $birthdayFields['birthdate'] . ') + (RIGHT(CURDATE(),5)>RIGHT(DATE(a.' . $birthdayFields['birthdate'] . '),5)))
-                        YEAR, CURDATE()) AS till');
-        if ($this->params->get('displayage'))
-            $query->select('(YEAR(CURDATE()) - YEAR(a.' . $birthdayFields['birthdate'] . ') + (RIGHT(CURDATE(),5)>RIGHT(DATE(a.' . $birthdayFields['birthdate'] . '),5))) AS age');
-        $query->from($birthdayFields['fromtable'] . ' AS a')
-            ->innerJoin('#__users AS b ON a.' . $birthdayFields['userid'] . ' = b.id' . $jomsocial)
-            ->having('till >= 0')
-            ->having('till <= ' . $this->params->get('nextxdays'))
-            ->order('till');
-
-        $query->order($db->escape($birthdayFields['$order']));
-        $db->setQuery($query, 0, $this->params->get('limit'));
-        $res = '';
-        try {
-            $res = $db->loadAssocList();
-        } catch (RuntimeException $e) {
-            JLog::add('Can\'t load user birthdates!', JLog::ERROR, 'mod_sw_kbirthday');
+        switch ($this->params->get('calcinterval')) {
+            case 'oneperday':
+                return $this->calcBirthdays();
+                break;
+            case 'eachtime':
+            default:
+                return $this->getBirthdayData();
+                break;
         }
-        if (!empty($res)) {
-            //setting up the right birthdate
-            //$todayyear = $this->time_now->format('Y', true);
-            foreach ($res as $k => $v) {
-                if ($v['year'] == 1 || empty($v['year'])) {
-                    unset($res[$k]);
-                } else {
-                    //DON'T USE OFFSET! because the birthdate is saved without time 0:00-2h is a day earlier which is wrong!
-                    $res[$k]['birthdate'] = new JDate($v['year'] . '-' . $v['month'] . '-' . $v['day']);
-                    $res[$k]['correction'] = 0;
-                    //both are leapyears or both are not
-                    if ($this->time_now->format('L') == $res[$k]['birthdate']->format('L')) {
-                        $res[$k]['correction'] = 0;
-                    } //now leap year and birthday not
-                    elseif ($this->time_now->format('L') == 1 && $res[$k]['birthdate']->format('L') == 0 &&
-                        $res[$k]['birthdate']->format('m') > 2
-                    ) {
-                        //this value have to added to the birthdate!
-                        $res[$k]['correction'] = 1;
-                    } //now non leap year but birthday leap year
-                    elseif ($this->time_now->format('L') == 0 && $res[$k]['birthdate']->format('L') == 1 &&
-                        $res[$k]['birthdate']->format('m') > 2
-                    ) {
-                        //this value have to added to the birthdate!
-                        $res[$k]['correction'] = -1;
-                    }
-                }
-            }
-        }
-        return $res;
+
     }
 
     /**
@@ -301,5 +248,129 @@ abstract class ModSWKbirthdayHelper
         }
 
         return false;
+    }
+
+    /**
+     * Get the information from all users which have birthday in a defined space of time.
+     *
+     * @return mixed|string
+     */
+    private function getBirthdayData()
+    {
+        $db = JFactory::getDBO();
+        $query = $db->getQuery(true);
+        $query->select('b.username');
+        $query->select('b.name');
+        $query->select('b.id AS userid');
+        $query->select('b.email');
+        $jomsocial = '';
+
+        $birthdayFields = $this->integration->getBirthdayDatabaseFields();
+
+        $query->select('YEAR(a.' . $birthdayFields['birthdate'] . ') AS year')
+            ->select('MONTH(a.' . $birthdayFields['birthdate'] . ') AS month')
+            ->select('DAYOFMONTH(a.' . $birthdayFields['birthdate'] . ') AS day')
+            ->select('DAYOFYEAR(a.' . $birthdayFields['birthdate'] . ') AS yearday')
+            ->select('DATEDIFF(DATE(a.' . $birthdayFields['birthdate'] . ') +
+                        INTERVAL(YEAR(CURDATE()) - YEAR(a.' . $birthdayFields['birthdate'] . ') + (RIGHT(CURDATE(),5)>RIGHT(DATE(a.' . $birthdayFields['birthdate'] . '),5)))
+                        YEAR, CURDATE()) AS till');
+        if ($this->params->get('displayage'))
+            $query->select('(YEAR(CURDATE()) - YEAR(a.' . $birthdayFields['birthdate'] . ') + (RIGHT(CURDATE(),5)>RIGHT(DATE(a.' . $birthdayFields['birthdate'] . '),5))) AS age');
+        $query->from($birthdayFields['fromtable'] . ' AS a')
+            ->innerJoin('#__users AS b ON a.' . $birthdayFields['userid'] . ' = b.id' . $jomsocial)
+            ->having('till >= 0')
+            ->having('till <= ' . $this->params->get('nextxdays'))
+            ->order('till');
+
+        $query->order($db->escape($birthdayFields['$order']));
+        $db->setQuery($query, 0, $this->params->get('limit'));
+        $res = '';
+        try {
+            $res = $db->loadAssocList();
+        } catch (RuntimeException $e) {
+            JLog::add('Can\'t load user birthdates!', JLog::ERROR, 'mod_sw_kbirthday');
+        }
+        if (!empty($res)) {
+            //setting up the right birthdate
+            //$todayyear = $this->time_now->format('Y', true);
+            foreach ($res as $k => $v) {
+                if ($v['year'] == 1 || empty($v['year'])) {
+                    unset($res[$k]);
+                } else {
+                    //DON'T USE OFFSET! because the birthdate is saved without time 0:00-2h is a day earlier which is wrong!
+                    $res[$k]['birthdate'] = new JDate($v['year'] . '-' . $v['month'] . '-' . $v['day']);
+                    $res[$k]['correction'] = 0;
+                    //both are leapyears or both are not
+                    if ($this->time_now->format('L') == $res[$k]['birthdate']->format('L')) {
+                        $res[$k]['correction'] = 0;
+                    } //now leap year and birthday not
+                    elseif ($this->time_now->format('L') == 1 && $res[$k]['birthdate']->format('L') == 0 &&
+                        $res[$k]['birthdate']->format('m') > 2
+                    ) {
+                        //this value have to added to the birthdate!
+                        $res[$k]['correction'] = 1;
+                    } //now non leap year but birthday leap year
+                    elseif ($this->time_now->format('L') == 0 && $res[$k]['birthdate']->format('L') == 1 &&
+                        $res[$k]['birthdate']->format('m') > 2
+                    ) {
+                        //this value have to added to the birthdate!
+                        $res[$k]['correction'] = -1;
+                    }
+                }
+            }
+        }
+        return $res;
+    }
+
+    private function calcBirthdays()
+    {
+        $db = JFactory::getDbo();
+        $query = $db->getQuery(true);
+
+        $query->select('calcdate')
+            ->from('#__schuweb_birthday');
+        $timestamp = $db->setQuery($query)->loadResult();
+
+        $calcDate = new JDate($timestamp);
+        $todayDate = new JDate();
+        $diff = $calcDate->diff($todayDate);
+
+        if (empty($timestamp) || $diff->format('%a') != 0) {
+
+            $db->truncateTable('#__schuweb_birthday');
+
+            $listOfBirthdays = $this->getBirthdayData();
+
+            $insert = array();
+
+            foreach ($listOfBirthdays as $birthday) {
+                $insert[] = $birthday['userid']
+                    . ', ' . $birthday['till']
+                    . ', ' . $birthday['age']
+                    . ', ' . $db->q($birthday['birthdate']->format('Y-m-d'))
+                    . ', ' . $birthday['correction'];
+            }
+
+            $query = $db->getQuery(true);
+            $query->insert('#__schuweb_birthday')
+                ->columns('userid, daystill, age, birthdate, correction')
+                ->values($insert);
+
+            $db->setQuery($query)
+                ->execute();
+        }
+
+        //return the calculated list
+        $query = $db->getQuery(true);
+        $query->select('*')
+            ->from('#__schuweb_birthday');
+        $res = $db->setQuery($query)
+            ->loadAssocList();
+
+        foreach ($res as $k => $v) {
+            $res[$k]['birthdate'] = new JDate($v['birthdate']);
+        }
+
+        return $res;
     }
 }
--- mod_sw_kbirthday/install.sql
+++ mod_sw_kbirthday/install.sql
@@ -1,4 +1,4 @@
--- @package Joomla.Module
+  -- @package Joomla.Module
 -- @subpackage mod_sw_kbirthday
 -- @copyright Copyright (C) 2010-2013 Sven Schultschik. All rights reserved.
 -- @license GNU General Public License version 2 or later
@@ -16,4 +16,14 @@ CREATE TABLE IF NOT EXISTS `#__schuweb_birthday_message` (
   `userid` int(11) NOT NULL DEFAULT '0',
   `topicid` int(11) NOT NULL,
   PRIMARY KEY (`userid`)
+);
+
+-- @since 2.0.0
+CREATE TABLE IF NOT EXISTS `#__schuweb_birthday` (
+  `userid` int(11) NOT NULL,
+  `daystill` smallint(11) unsigned NOT NULL,
+  `age` tinyint(11) unsigned NOT NULL,
+  `birthdate` date NOT NULL,
+  `correction` tinyint(4) NOT NULL,
+  `calcdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
