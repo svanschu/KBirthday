8618139abaa297d579e554cee4a1fe85c59050bf
svanschu <sven@schultschik.de>
2015-06-03 19:59:06 +0200

    [#22] update routine for database

--- mod_sw_kbirthday/install.script.php
+++ mod_sw_kbirthday/install.script.php
@@ -15,24 +15,39 @@ class mod_sw_kbirthdayInstallerScript
     /**
      * Constructor
      *
-     * @param   JAdapterInstance  $adapter  The object responsible for running this script
+     * @param   JAdapterInstance $adapter The object responsible for running this script
      */
-    public function __construct($parent){
+    public function __construct($parent)
+    {
 
     }
 
     /**
      * Called before any type of action
      *
-     * @param   string  $route  Which action is happening (install|uninstall|discover_install|update)
-     * @param   JAdapterInstance  $adapter  The object responsible for running this script
+     * @param   string $route Which action is happening (install|uninstall|discover_install|update)
+     * @param   JAdapterInstance $adapter The object responsible for running this script
      *
      * @return  boolean  True on success
      */
-    public function preflight($route, $parent){
-        if (version_compare($this->getParam('version'), '1.9.0') < 0 ) {
-            $db = JFactory::getDbo();
+    public function preflight($route, $parent)
+    {
+        $db = JFactory::getDbo();
+
+        if (version_compare($this->getParam('version'), '1.9.0') < 0) {
             $db->setQuery("CREATE TABLE IF NOT EXISTS `#__schuweb_birthday_message` ( `userid` int(11) NOT NULL DEFAULT '0', `topicid` int(11) NOT NULL, PRIMARY KEY (`userid`));");
+            $db->execute();
+        }
+
+        if (version_compare($this->getParam('version'), '2.0.0') < 0) {
+            $db->setQuery("CREATE TABLE IF NOT EXISTS `#__schuweb_birthday` (
+  `userid` int(11) NOT NULL,
+  `daystill` smallint(11) unsigned NOT NULL,
+  `age` tinyint(11) unsigned NOT NULL,
+  `birthdate` date NOT NULL,
+  `correction` tinyint(4) NOT NULL,
+  `calcdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+);");
             $db->execute();
         }
     }
@@ -40,49 +55,54 @@ class mod_sw_kbirthdayInstallerScript
     /**
      * Called after any type of action
      *
-     * @param   string  $route  Which action is happening (install|uninstall|discover_install|update)
-     * @param   JAdapterInstance  $adapter  The object responsible for running this script
+     * @param   string $route Which action is happening (install|uninstall|discover_install|update)
+     * @param   JAdapterInstance $adapter The object responsible for running this script
      *
      * @return  boolean  True on success
      */
-    public function postflight($route, $parent){
+    public function postflight($route, $parent)
+    {
 
     }
 
     /**
      * Called on installation
      *
-     * @param   JAdapterInstance  $adapter  The object responsible for running this script
+     * @param   JAdapterInstance $adapter The object responsible for running this script
      *
      * @return  boolean  True on success
      */
-    public function install($parent){
+    public function install($parent)
+    {
 
     }
 
     /**
      * Called on update
      *
-     * @param   JAdapterInstance  $adapter  The object responsible for running this script
+     * @param   JAdapterInstance $adapter The object responsible for running this script
      *
      * @return  boolean  True on success
      */
-    public function update($parent){
+    public function update($parent)
+    {
     }
 
     /**
      * Called on uninstallation
      *
-     * @param   JAdapterInstance  $adapter  The object responsible for running this script
+     * @param   JAdapterInstance $adapter The object responsible for running this script
      */
-    public function uninstall($parent){
+    public function uninstall($parent)
+    {
 
     }
 
-    function getParam( $name ) {
+    function getParam($name)
+    {
         $db = JFactory::getDbo();
         $db->setQuery('SELECT manifest_cache FROM #__extensions WHERE name = "mod_sw_kbirthday"');
-        $manifest = json_decode( $db->loadResult(), true );
-        return $manifest[ $name ];
+        $manifest = json_decode($db->loadResult(), true);
+        return $manifest[$name];
     }
 }
